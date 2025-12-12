0ee0ea208ede2a6f62a192d9ba2bcafacf501ef8
chronoDave <davidwolters6758@gmail.com>
2025-12-12 11:11:13 +0000

    0.7.10

--- dist/eslint-config.js
+++ dist/eslint-config.js
@@ -21,9 +21,20 @@ var rules2 = rule_default("import-x")({
   "no-deprecated": "warn",
   "no-empty-named-blocks": "error",
   "no-extraneous-dependencies": ["error", {
-    optionalDependencies: ["**/*.spec.*", "**/*.fixture.*", "**/*.config.*"],
-    devDependencies: ["**/*.spec.*", "**/*.fixture.*", "**/*.config.*"],
-    whitelist: ["esbuild", "electron"]
+    optionalDependencies: [
+      "**/*.spec.*",
+      "**/*.fixture.*",
+      "**/*.config.*",
+      "test/**/*",
+      "scripts/**/*"
+    ],
+    devDependencies: [
+      "**/*.spec.*",
+      "**/*.fixture.*",
+      "**/*.config.*",
+      "test/**/*",
+      "scripts/**/*"
+    ]
   }],
   "no-mutable-exports": "error",
   "no-named-as-default": "off",
